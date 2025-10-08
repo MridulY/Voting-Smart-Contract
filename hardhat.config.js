d0579b30760501f85ff6be38313b8bbfcc1ff344
MridulY <mridulyadav3173@gmail.com>
2025-10-08 22:41:19 +0530

    Init Hardhat Config

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const { SEPOLIA_RPC_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
+
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL || "",
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY || ""
+  }
+};
